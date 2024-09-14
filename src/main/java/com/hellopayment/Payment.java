ba427bf761fe60a92850311ce6a214d2d132f9be
iyeonghun <twosoull@naver.com>
2024-09-14 21:20:47 +0900

    Initial commit

---
+++ src/main/java/com/hellopayment/Payment.java
@@ -0,0 +1,4 @@
+package com.hellopayment;
+
+public class Payment {
+}
---
+++ src/main/java/com/hellopayment/PaymentService.java
@@ -0,0 +1,16 @@
+package com.hellopayment;
+
+public class PaymentService {
+
+    public Payment prepare() {
+        return new Payment();
+    }
+
+    public static void main(String[] args) {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare();
+        System.out.println(payment);
+
+    }
+
+}
