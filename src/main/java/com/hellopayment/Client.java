51151c1db0d64f22873c806f53c19dd4c7713286
iyeonghun <twosoull@naver.com>
2024-09-23 23:09:15 +0900

    관심사 분리 끝. BeanFactory의 사용

--- src/main/java/com/hellopayment/Client.java
+++ src/main/java/com/hellopayment/Client.java
@@ -1,13 +1,18 @@
 package com.hellopayment;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
         //PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
-        ObjectFactory factory = new ObjectFactory();
-        PaymentService paymentService = factory.paymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+
+        //paymentService를 가져오는 부분
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
